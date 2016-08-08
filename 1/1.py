8ca919dabe1e245750b5ac8667e9bb1aca8dc1e8
River Zhou <riverzhou2000@163.com>
2016-08-08 10:51:06 +0800

    update case 1 for plaint

--- 1/1.py
+++ 1/1.py
@@ -3,6 +3,8 @@
 from sklearn import datasets
 from sklearn import linear_model
 
+import matplotlib.pyplot as plt
+
 #Load Train and Test datasets
 #Identify feature and response variable(s) and values must be numeric and numpy arrays
 
@@ -11,13 +13,14 @@ boston = datasets.load_boston()
 x_train=boston.data[:,5].reshape(-1,1)
 y_train=boston.target.reshape(-1,1)
 x_test=boston.data[:50,5].reshape(-1,1)
+y_test=boston.target[:50].reshape(-1,1)
 
 # Create linear regression object
 linear = linear_model.LinearRegression()
 
 # Train the model using the training sets and check score
 linear.fit(x_train, y_train)
-linear.score(x_train, y_train)
+print('Regress score :', linear.score(x_train, y_train))
 
 #Equation coefficient and Intercept
 print('Coefficient: n', linear.coef_)
@@ -26,5 +29,11 @@ print('Intercept: n', linear.intercept_)
 #Predict Output
 predicted= linear.predict(x_test)
 
-print('predicted', predicted)
+for i in range(len(x_test)) :
+        print(x_test[i], y_test[i], predicted[i])
+#print('predicted', predicted)
+
+plt.scatter(y_test,predicted)
+plt.plot([y_test.min(), y_test.max()],[y_test.min(), y_test.max()])
+plt.show()
 
